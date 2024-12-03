10ce9a9ec89a549e0fa885fc60f035bb0783b455
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-02 17:06:47 -0800

    Added React

---
+++ stephenjlu/.storybook/preview.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/react').Preview } */
+
+import "../src/styles/uikit.css";
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
